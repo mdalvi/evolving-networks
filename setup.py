a0ea169dd2d3186e7b3c12c95efd8c865b69bbc8
Milind Dalvi <milind.dalvi14@gmail.com>
2018-05-27 00:10:31 +0530

    activations added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='evolving_networks',
+    version='0.01',
+    packages=['notebooks', 'evolving_networks'],
+    url='https://turingequations.com',
+    license='GNU GENERAL PUBLIC LICENSE 3',
+    author='mdalvi',
+    author_email='milind.dalvi@turingequations.com',
+    description=''
+)
