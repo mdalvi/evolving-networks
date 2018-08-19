42656c269317f0b35697ef353b7a50fe906e662b
Milind Dalvi <milind.dalvi14@gmail.com>
2018-08-19 18:18:27 +0530

    activations revised

--- evolving_networks/activations.py
+++ evolving_networks/activations.py
@@ -3,137 +3,119 @@
 # References
 # ==============
 
-[1] https://en.wikipedia.org/wiki/Rectifier_(neural_networks)
-[2] https://en.wikipedia.org/wiki/Activation_function
-[3] https://arxiv.org/pdf/1710.09967.pdf
-[4] https://en.wikipedia.org/wiki/Sinc_function
-[5] https://github.com/keras-team/keras/blob/master/keras/activations.py
+[1] https://pytorch.org/docs/stable/nn.html#hardtanh
+[2] https://pytorch.org/docs/stable/nn.html#relu6
+[3] https://pytorch.org/docs/stable/nn.html#elu
+[4] https://pytorch.org/docs/stable/nn.html#selu
+[5] https://pytorch.org/docs/stable/nn.html#leakyrelu
+[6] https://pytorch.org/docs/stable/nn.html#prelu
+[7] https://pytorch.org/docs/stable/nn.html#rrelu
+[8] https://pytorch.org/docs/stable/nn.html#logsigmoid
+[9] https://pytorch.org/docs/stable/nn.html#hardshrink
+[10] https://pytorch.org/docs/stable/nn.html#tanhshrink
+[11] https://pytorch.org/docs/stable/nn.html#softsign
+[12] https://pytorch.org/docs/stable/nn.html#softplus
+[13] https://pytorch.org/docs/stable/nn.html#softshrink
+[14] https://pytorch.org/docs/stable/nn.html#tanh
+[15] https://pytorch.org/docs/stable/nn.html#sigmoid
+[16] https://pytorch.org/docs/stable/nn.html#relu
 
 """
 
 import math
+import random
 
 from evolving_networks.errors import InvalidActivationError
 
 
-def identity_activation(z):
-    return z
+def identity_activation(x):
+    return x
 
 
-def binary_step_activation(z):  # [2]
-    return 1.0 if z > 0.0 else 0.0
+def hard_tanh_activation(x):
+    return 1.0 if x > 1 else -1.0 if x < -1 else x  # [1]
 
 
-def sigmoid_activation(z):
-    return 1.0 / (1.0 + math.exp(-z))
+def relu6_activation(x):
+    return min(max(0, x), 6)  # [2]
+
+
+def elu_activation(x, alpha=1.0):
+    return max(0.0, x) + min(0.0, alpha * (math.exp(x) - 1.0))  # [3]
+
+
+def selu_activation(x, alpha=1.6732632423543772848170429916717, scale=1.0507009873554804934193349852946):  # [3], [4]
+    return scale * elu_activation(x, alpha)
+
+
+def leaky_relu_activation(x, negative_slope=0.01):
+    return max(0.0, x) + (negative_slope * min(0.0, x))  # [5]
+
+
+def prelu_activation(x, init=0.25):
+    return max(0.0, x) + (init * min(0, x))  # [6]
+
+
+def rrelu_activation(x, lower=0.125, upper=0.3333333333333333):
+    return max(0.0, x) + (random.uniform(lower, upper) * min(0, x))  # [7]
+
+
+def log_sigmoid_activation(x):
+    return math.log(1.0 / (1.0 + math.exp(-1.0 * x)))  # [8]
+
+
+def hard_shrink_activation(x, _lambda=0.5):
+    return x if x > _lambda else x if x < (-1.0 * _lambda) else 0.0  # [9]
+
+
+def tanh_shrink_activation(x):
+    return x - tanh_activation(x)  # [10]
+
+
+def soft_sign_activation(x):
+    return x / (1.0 + abs(x))  # [11]
+
+
+def soft_plus_activation(x, beta=1.0, threshold=20.0):
+    return x if x > threshold else ((1.0 / beta) * math.log(1 + (math.exp(beta * x))))  # [12]
+
+
+def soft_shrink_activation(x, _lambda=0.5):
+    return x - _lambda if x > _lambda else x + _lambda if x < (-1.0 * _lambda) else 0.0  # [13]
 
 
 def tanh_activation(z):
-    return math.tanh(z)
+    return math.tanh(z)  # [14]
 
 
-def arc_tan_activation(z):  # [2]
-    return math.atan(z)
+def sigmoid_activation(x):
+    return 1.0 / (1.0 + math.exp(-1.0 * x))  # [15]
 
 
-def sin_activation(z):
-    return math.sin(z)
-
-
-def soft_sign_activation(z):  # [2]
-    return z / (1.0 + abs(z))
-
-
-def inverse_square_root_unit_activation(z, alpha=3.0):  # [2], [3]
-    return z / math.sqrt(1.0 + alpha * (z ** 2))
-
-
-def relu_activation(z):
-    return z if z > 0.0 else 0.0
-
-
-def leaky_relu_activation(z):
-    return z if z > 0.0 else 0.01 * z
-
-
-def elu_activation(z, alpha=1.0):
-    return z if z > 0.0 else alpha * (math.exp(z) - 1.0)
-
-
-def selu_activation(z, alpha=1.6732632423543772848170429916717, scale=1.0507009873554804934193349852946):  # [2], [5]
-    return scale * elu_activation(z, alpha)
-
-
-def inverse_square_root_linear_unit_activation(z, alpha=3.0):
-    return z if z > 0.0 else inverse_square_root_unit_activation(z, alpha)
-
-
-def soft_plus_activation(z):  # [1], [2]
-    return math.log(1 + math.exp(z))
-
-
-def bent_activation(z):  # [2]
-    return ((math.sqrt((z ** 2) + 1.0) - 1.0) / 2.0) + z
-
-
-def sinc_activation(z):  # [4]
-    return 1.0 if z == 0.0 else math.sin(z) / z
-
-
-def gaussian_activation(z):
-    return math.exp(-1.0 * (z ** 2))
-
-
-def clamped_activation(z):
-    return max(-1.0, min(1.0, z))
-
-
-def inverse_activation(z):
-    try:
-        z = 1.0 / z
-    except ArithmeticError:
-        return 0.0
-    else:
-        return z
-
-
-def exp_activation(z):
-    return math.exp(z)
-
-
-def abs_activation(z):
-    return abs(z)
-
-
-def hat_activation(z):
-    return max(0.0, 1 - abs(z))
+def relu_activation(x):
+    return max(0.0, x)  # [16]
 
 
 class ActivationFunctionSet(object):
     def __init__(self):
         self.functions = {}
-        self._add('sigmoid', sigmoid_activation)
-        self._add('tanh', tanh_activation)
-        self._add('sin', sin_activation)
-        self._add('gauss', gaussian_activation)
-        self._add('relu', relu_activation)
-        self._add('softplus', soft_plus_activation)
         self._add('identity', identity_activation)
-        self._add('clamped', clamped_activation)
-        self._add('inv', inverse_activation)
-        self._add('exp', exp_activation)
-        self._add('abs', abs_activation)
-        self._add('hat', hat_activation)
-        self._add('step', binary_step_activation)
-        self._add('arctan', arc_tan_activation)
-        self._add('softsign', soft_sign_activation)
-        self._add('isrua', inverse_square_root_unit_activation)
-        self._add('lrelu', leaky_relu_activation)
+        self._add('hardtanh', hard_tanh_activation)
+        self._add('relu6', relu6_activation)
         self._add('elu', elu_activation)
         self._add('selu', selu_activation)
-        self._add('isrlua', inverse_square_root_linear_unit_activation)
-        self._add('bent', bent_activation)
-        self._add('sinc', sinc_activation)
+        self._add('lrelu', leaky_relu_activation)
+        self._add('prelu', prelu_activation)
+        self._add('rrelu', rrelu_activation)
+        self._add('logsigmoid', log_sigmoid_activation)
+        self._add('hardshrink', hard_shrink_activation)
+        self._add('tanhshirnk', tanh_shrink_activation)
+        self._add('softsign', soft_sign_activation)
+        self._add('softplus', soft_plus_activation)
+        self._add('softshrink', soft_shrink_activation)
+        self._add('tanh', tanh_activation)
+        self._add('sigmoid', sigmoid_activation)
+        self._add('relu', relu_activation)
 
     def _add(self, name, func):
         self.functions[name] = func
--- evolving_networks/activations/activation.py
+++
@@ -1,6 +0,0 @@
-class Activation(object):
-    def __init__(self):
-        pass
-
-    def activate(self, z):
-        raise NotImplementedError()
