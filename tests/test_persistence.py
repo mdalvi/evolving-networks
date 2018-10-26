cc7abdf236ed32ee0f7eae26f4a14d510f35e177
Milind Dalvi <milind.dalvi14@gmail.com>
2018-10-27 02:09:02 +0530

    test cases updated

--- tests/test_persistence.py
+++ tests/test_persistence.py
@@ -7,18 +7,18 @@ from evolving_networks.genome.genome import Genome
 class TestPersistence(unittest.TestCase):
     def test_genome_persistence(self):
         config = Config()
-        config.initialize('persistence_config_1.ini')
+        config.initialize('test_persistence_1.ini')
         old_g = Genome(g_id=None, generation=None, config=config.genome)
         old_g.initialize(config.node, config.connection)
         old_g_json = old_g.to_json()
-        new_g = Genome(g_id=None, generation=None, config=config.genome)
+        new_g = Genome(g_id=None, generation=None, config=None)
         new_g.from_json(old_g_json)
 
         assert (old_g == new_g)
 
     def test_config_persistence(self):
         old_config = Config()
-        old_config.initialize('persistence_config_1.ini')
+        old_config.initialize('test_persistence_1.ini')
         config_json = old_config.to_json()
         new_config = Config()
         new_config.from_json(config_json)
